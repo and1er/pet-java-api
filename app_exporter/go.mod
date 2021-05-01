60cb82f7f202c05f33450beccbe8e0983829cf37
and1er <34434784+and1er@users.noreply.github.com>
2021-05-01 03:14:45 +0300

    Simple prometheus golang metrics exporter

--- app_exporter/go.mod
+++ app_exporter/go.mod
@@ -1,3 +1,5 @@
 module github.com/and1er/pet-java-api/app_exporter
 
 go 1.16
+
+require github.com/prometheus/client_golang v1.10.0
--- app_exporter/main.go
+++ app_exporter/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World!")
-	})
+	http.Handle("/metrics", promhttp.Handler())
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
